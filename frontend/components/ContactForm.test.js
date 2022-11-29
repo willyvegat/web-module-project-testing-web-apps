0946ae522e74b63d3df320e76dc67efd8b227243
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-11-28 23:07:25 -0500

    error message for empty lastName field

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -65,9 +65,14 @@ test('renders "email must be a valid email address" if an invalid email is enter
     expect(errorMessage).toBeInTheDocument();
 });
 
-// test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
+test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
+    render(<ContactForm/>);
+    const submitBtn = screen.getByRole('button');
+    userEvent.click(submitBtn);
 
-// });
+    const errorMessage = await screen.findByText(/lastName is a required field/i);
+    expect(errorMessage).toBeInTheDocument();
+});
 
 // test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
 
